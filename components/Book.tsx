f8311c9121dfa45944e091cc3863f6d8a4559147
Karabin13 <ivan.karabinskiy1308@gmail.com>
2022-11-15 08:24:02 +0200

    fix

--- components/Book.tsx
+++ components/Book.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import { Books } from '../types';
-import { contentfulLoader } from '@delicious-simplicity/next-image-contentful-loader';
 
 const Book = ({ books }: Books) => {
   const posterLink = `http://${books.fields.poster?.fields.file.url}`;
@@ -11,14 +10,7 @@ const Book = ({ books }: Books) => {
 
   return (
     <div className="book">
-      <Image
-        loader={(props) => contentfulLoader(props, { fit: 'crop', ar: '1:1' })}
-        className="poster"
-        src={posterLink}
-        width={270}
-        height={400}
-        alt="poster"
-      />
+      <Image className="poster" src={posterLink} width={270} height={400} alt="poster" />
       <div className="titlebook">{books.fields.booktitle}</div>
       <Link href={showDescription}>
         <button className="button">{books.fields.button}</button>
